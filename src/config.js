518b511674714830526893aaad181b9d1d9213ce
Brannen Petit <bpetit940@gmail.com>
2020-04-24 08:50:00 -0500

    added api endpoint string

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
-  API_ENDPOINT: "http://localhost:8000/api",
+  API_ENDPOINT:
+    "dbname=d4ievu9gm468sv host=ec2-18-210-214-86.compute-1.amazonaws.com port=5432 user=vigzpvdoyozdlf password=f657590bc90d8948b60b3911cad74adabacc4e92605a7e42185799b44ebad592 sslmode=require",
   TOKEN_KEY: "nutred-client-auth-token",
 };
